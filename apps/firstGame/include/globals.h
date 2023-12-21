725c51d8f9ff8acd020b1509dbe18c16b78592c3
Josh Hamill <joshua@transposit.com>
2023-12-20 21:10:05 -0500

    feat: remove unused "globals" file

--- apps/firstGame/include/globals.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <glm/glm.hpp>
-#include <vector>
-
-extern glm::vec3 g_lightDirection;
-
--- apps/firstGame/src/globals.cpp
+++
@@ -1,4 +0,0 @@
-#include "globals.h"
-
-glm::vec3 g_lightDirection(-0.2f, -1.0f, -0.3f);
-
